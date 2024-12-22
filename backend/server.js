60f5f1c42a04324b71d7c079a8dc3902b6c89580
Manish Basnet <mb2060127@gmail.com>
2024-12-22 11:06:04 +0545

    mh

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
+app.use("*",cors({
   origin: ['http://localhost:5173','https://llov-frontend.vercel.app'], // Frontend domains
   methods: ['GET, POST'],
   allowedHeaders: ['Content-Type'],
