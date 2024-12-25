e2fc2dfadb90f9baf2069dab0a6a7933be89595e
Manish Basnet <mb2060127@gmail.com>
2024-12-25 08:51:38 +0545

    ms

--- frontend/src/DataPage.jsx
+++ frontend/src/DataPage.jsx
@@ -13,7 +13,7 @@ const DataPage = () => {
     const fetchEntries = async () => {
       try {
         const response = await axios.get("http://localhost:5000/api/entries");
-          //  const response = await axios.get("https://llov.vercel.app/api/entries");
+          //  const response = await axios.get("https://love-calculator-two-brown.vercel.app/api/entries");
    
         setEntries(response.data); // Set the fetched entries into state
       } catch (error) {
